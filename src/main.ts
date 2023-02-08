e74f2bbf94a9dabe3dd1adf416633217d8270e6e
Geferson <gefersonjefreey@gmail.com>
2023-02-08 18:47:11 -0300

    feat: fix cors in mobile

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import cors from 'cors';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, { cors: true });
@@ -43,6 +44,7 @@ async function bootstrap() {
         );
         next();
     });
+    app.use(cors({ origin: '*' }));
 
     const config = new DocumentBuilder()
         .setTitle('API NDM')
