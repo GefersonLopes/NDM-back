5b713e2b4c269aeec19ed7aba6d73630d2957094
Geferson <gefersonjefreey@gmail.com>
2023-02-08 14:20:06 -0300

    feat: alter host of database

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -2,7 +2,7 @@ import { TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 export const postgresConfig: TypeOrmModuleOptions = {
     type: 'postgres',
-    host: '18.231.114.17',
+    host: process.env.DATABASE_HOST,
     port: 5432,
     username: 'postgres',
     password: '1234',
